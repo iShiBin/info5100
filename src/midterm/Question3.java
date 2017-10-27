01df393e50474221a7f12141b75229aeb5cda126
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-10-27 13:32:21 -0700

    score update

--- src/midterm/Question3.java
+++ src/midterm/Question3.java
@@ -1,3 +1,4 @@
+//score 7
 /**
 Given a non­empty integer array of size n, find the minimum number of moves required to make all array elements equal,
 where a move is incrementing n ­ 1 elements by 1.
