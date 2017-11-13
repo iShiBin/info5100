dfb58ca247a8fabd8fc087f87d750a500e3c365b
Bin Shi <i@shibin.info>
2017-11-12 19:13:51 -0800

    add code to deal with negative numbers in the array

--- src/assignment7/Partition.java
+++ src/assignment7/Partition.java
@@ -11,42 +11,45 @@ import java.util.stream.*;
  * {1, 5, 5} and {11} arr[] = {1, 5, 3} Output: false The array cannot be
  * partitioned into equal sum sets
  * 
+ * Note: The numbers could be negative.
+ * 
  * @author bin
  */
 class Partition {
   public boolean findPartition(int arr[]) {
+    if(arr==null || arr.length<2) return false;
+    
+ // the target is to find a group of number which sum to half
     long sum = IntStream.of(arr).asLongStream().sum();
-
-    // sum is not even, return false
-    if (sum % 2 == 1)
-      return false;
-
-    long target = (sum >> 1); // the target is to find a group of number which
-                              // sum to half
-    Arrays.sort(arr);
-    return findPartition(arr, arr.length - 2, target - arr[arr.length - 1]);
+    
+//    if sum is odd, there can not be two subsets with equal sum
+    if(sum%2==1) return false;
+    
+    return findPartition(arr, arr.length-1, sum>>1);
   }
 
-  private boolean findPartition(int[] nums, int index, long target) {
-    if (target == 0) {
-      return true;
-    }
-    if (target < 0 || index < 0) {
-      return false;
-    }
+  private boolean findPartition(int[] nums, int tail, long target) {
+    if (target == 0) return true;
+    if (tail < 0 ) return false;
+
     // if the biggest is not the target, then find it in the smaller numbers or
     // find the remainder in the smaller numbers.
-    return findPartition(nums, index - 1, target - nums[index]) || findPartition(nums, index - 1, target);
+    return findPartition(nums, tail - 1, target - nums[tail]) || findPartition(nums, tail - 1, target);
   }
 
+//  main function to test/verify
   public static void main(String[] args) {
     int[] nums = { 1, 5, 11, 5 };
     System.out.println(new Partition().findPartition(nums));
 
     nums = new int[] { 1, 5, 3 };
     System.out.println(new Partition().findPartition(nums));
-
-    nums = new Random().ints(100, 1, 1000).toArray();
+    
+    nums = new int[]{-1,2,1};
+    System.out.println(new Partition().findPartition(nums));
+    
+//    generate an integer array with 100 numbers range from -100 to 100
+    nums = new Random().ints(100,-100, 100).toArray();
     System.out.println(new Partition().findPartition(nums));
   }
 }
